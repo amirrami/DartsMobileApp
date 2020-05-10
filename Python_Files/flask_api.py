4c3853aab2ff7d791f8329ce321a37f4176e2948
andrewawad <andrew_usa90@hotmail.com>
2020-05-10 21:57:12 +0200

    adding api

---
+++ Python_Files/flask_api.py
@@ -0,0 +1,84 @@
+from flask import Flask, send_from_directory, redirect,request
+import os
+from Main_darts import setBackgroundImage,getScore
+app = Flask(__name__)
+
+from gevent.pywsgi import WSGIServer
+
+from PIL import Image
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def main():
+    return redirect("https://github.com/gxercavins/image-api/blob/master/README.md", code=302)
+
+
+# upload selected image and forward to processing page
+@app.route("/get_dart_board_image", methods=["POST"])
+def get_dartBoard():
+    target = os.path.join(APP_ROOT, 'static/images/')
+
+    # retrieve file from html file-picker
+    upload = request.files.getlist("datBoard_image")[0]
+    print("File name: {}".format(upload.filename))
+    filename = upload.filename
+
+    # file support verification
+    ext = os.path.splitext(filename)[1]
+    if (ext == ".jpg") or (ext == ".png") or (ext == ".bmp"):
+        print("File accepted")
+    else:
+        return False
+
+    # save file
+    destination = "/".join([target, filename])
+    print("File saved to to:", destination)
+    upload.save(destination)
+
+
+    # forward to processing page
+    if setBackgroundImage(destination):
+        return "Done"
+    else:
+        return "Please take another Board Image" 
+
+# upload selected image and forward to processing page
+@app.route("/get_dart_score", methods=["POST"])
+def get_score():
+    target = os.path.join(APP_ROOT, 'static/images/')
+
+    # retrieve file from html file-picker
+    upload = request.files.getlist("dart_image")[0]
+    print("File name: {}".format(upload.filename))
+    filename = upload.filename
+
+    # file support verification
+    ext = os.path.splitext(filename)[1]
+    if (ext == ".jpg") or (ext == ".png") or (ext == ".bmp"):
+        print("File accepted")
+    else:
+        return False
+
+    # save file
+    destination = "/".join([target, filename])
+    print("File saved to to:", destination)
+    upload.save(destination)
+
+    score , outputImage = getScore(destination)
+    # forward to processing page
+    if score is False:
+        return "Please take another dart Image"
+    else:
+        return send_image(outputImage)#{"score":score} 
+
+# retrieve file from 'static/images' directory
+@app.route('/static/images/<filename>')
+def send_image(filename):
+    return send_from_directory("static/images", filename)
+
+
+
+if __name__ == "__main__":
+    http_server = WSGIServer(('192.168.1.15', 5000), app)
+    http_server.serve_forever()
